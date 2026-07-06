594b423da881e272ebb015a8b291177260050e30
Duy Tân <tanlpd684783@ut.edu.vn>
2026-07-06 15:36:08 +0700

    Update data analysis module

--- src/analysis.py
+++ src/analysis.py
@@ -11,10 +11,10 @@ df["Datetime"] = pd.to_datetime(
 print(df["Datetime"].head())
 
 #tính thời gian từ lúc đăng bài đến hiện tại
-now = datetime.now()  
+now = pd.Timestamp.now()
 df["Hours_Elapsed"] = (
-    now - df["Datetime"]
-).dt.total_seconds() / 3600
+    (now - df["Datetime"]) / pd.Timedelta(hours=1)
+)
 print(df["Hours_Elapsed"].head())
 
 #tính tổng likes và retweets theo từng Hashtag, từng Platform
